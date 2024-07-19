5d91818a9daece900d228f2a1bb5003615d509b2
九渊 <1058477702@qq.com>
2024-07-19 20:19:09 +0800

    feat: 新增享元模式示例

---
+++ src/structual/flyweight/code/shape/Circle.java
@@ -0,0 +1,33 @@
+package structual.flyweight.code.shape;
+
+
+import structual.flyweight.code.shape.abstract_shape.Shape;
+
+public class Circle implements Shape {
+    private String color;
+    private int x;
+    private int y;
+    private int radius;
+
+    public Circle(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Circle: Draw() [Color : " + color + ", x : " + x + ", y :" + y + ", radius :" + radius);
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+}
---
+++ src/structual/flyweight/code/shape/abstract_shape/Shape.java
@@ -0,0 +1,5 @@
+package structual.flyweight.code.shape.abstract_shape;
+
+public interface Shape {
+    void draw();
+}
